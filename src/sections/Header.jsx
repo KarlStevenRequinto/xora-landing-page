4670818de4c1e56d65be0e75df2319a4ca5ba008
KarlStevenRequinto <lerdrobott@gmail.com>
2025-01-04 03:05:39 +0800

    header done

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,7 +1,9 @@
 import { Link as LinkScroll } from "react-scroll";
 import { useState } from "react";
+import clsx from "clsx";
+
 const NavLink = ({ title }) => (
-    <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
+    <LinkScroll to="" className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
         {title}
     </LinkScroll>
 );
@@ -15,7 +17,12 @@ const Header = () => {
                 <a className="lg:hidden flex-1 cursor-pointer z-2">
                     <img src="/images/xora.svg" width={115} height={55} alt="logo" />
                 </a>
-                <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+                <div
+                    className={clsx(
+                        "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
+                        isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"
+                    )}
+                >
                     <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
                         <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
                             <ul className="flex max-lg:block max-lg:px-12">
@@ -26,7 +33,13 @@ const Header = () => {
                                 </li>
 
                                 <li className="nav-logo">
-                                    <LinkScroll>
+                                    <LinkScroll
+                                        to="hero"
+                                        offset={-100}
+                                        smooth
+                                        spy
+                                        className={clsx("max-lg:hidden transition-transform duration-500 cursor-pointer")}
+                                    >
                                         <img src="/images/xora.svg" width={160} height={55} alt="logo" />
                                     </LinkScroll>
                                 </li>
@@ -38,13 +51,18 @@ const Header = () => {
                                 </li>
                             </ul>
                         </nav>
+
+                        <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h=[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+                            <img src="/images/bg-outlines.svg" alt="outline" width={960} height={380} className="relative z-2" />
+                            <img src="/images/bg-outlines-fill.png" alt="outline" width={960} height={380} className="absolute inset-0 mix-blend-soft-light opacity-5" />
+                        </div>
                     </div>
                 </div>
                 <button
                     className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
                     onClick={() => isSetOpen((prevState) => !prevState)}
                 >
-                    <img src={`/images/${isOpen ? "close" : "magic"}.svg`} />
+                    <img src={`/images/${isOpen ? "close" : "magic"}.svg`} alt="magic" className="size-1/2 object-contain" />
                 </button>
             </div>
         </header>
