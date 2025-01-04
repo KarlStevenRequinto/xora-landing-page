5f9a9dbd354fba1bc1a0bc9a94be72cd002ad404
KarlStevenRequinto <lerdrobott@gmail.com>
2025-01-05 03:03:47 +0800

    adding Pricing and Faq sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import Faq from "./sections/Faq";
 import Features from "./sections/Features";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
+import Pricing from "./sections/Pricing";
 
 const App = () => {
     return (
@@ -8,6 +10,8 @@ const App = () => {
             <Header />
             <Hero/>
             <Features/>
+            <Pricing/>
+            <Faq/>
         </main>
     );
 };
